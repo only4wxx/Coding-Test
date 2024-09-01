bc0eaf62bace3ac864f219f988bf5ab78cc722ba
박연우 <125821804+only4wxx@users.noreply.github.com>
2024-09-01 18:40:24 +0900

    [Silver III] Title: Four Squares, Time: 240 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -14,19 +14,18 @@ memo[2] = 2
 memo[3] = 3
 
 def find_square(num):
-    if num in memo.keys(): return memo[num]
+    if num in memo.keys(): return memo[num] # 값을 이미 구했었다면
 
-    max_square = int(num**0.5)
+    max_square = int(num**0.5) # 제곱수의 합이 되어야 하므로 가장 큰 제곱수를 구함
     # print('num: '+str(num), end=' / ')
     # print(max_square)
 
     min_result = 0
-    for n in range(max_square, max_square//2-1, -1):
+    for n in range(max_square, max_square//2-1, -1): # 최소 개수를 구함
         result_square = find_square(num-n**2)
-        if result_square > 0:
-            if not min_result: min_result = 1 + result_square
-            elif result_square+1 < min_result: min_result = 1 + result_square
-    if min_result: memo[num] = min_result
+        if not min_result: min_result = 1 + result_square
+        elif result_square+1 < min_result: min_result = 1 + result_square
+    memo[num] = min_result
     return min_result
 
 print(find_square(N))

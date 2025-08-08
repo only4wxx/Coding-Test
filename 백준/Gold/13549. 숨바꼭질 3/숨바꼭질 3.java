df8dd42fc1d97cb5b61f3419b53b716a7ff131f3
Yeonwoo Park <125821804+only4wxx@users.noreply.github.com>
2025-08-08 20:45:09 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 256 ms, Memory: 95136 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -3,9 +3,7 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
